d9214500db1dd363f8d106d1d50fe61265285926
julian castro <jucastrohenao@gmail.com>
2025-01-22 09:47:00 -0500

    fixing projects page

--- src/projects/MyProyects.jsx
+++ src/projects/MyProyects.jsx
@@ -13,7 +13,11 @@ export default function MyProyects(){
         useEffect(()=>{
             const urlProyectos= ["https://img.freepik.com/vector-gratis/ilustracion-planta-frailejon-diseno-plano_23-2150043617.jpg",
                 "https://img.freepik.com/vector-gratis/ilustracion-planta-frailejon-diseno-plano_23-2150043617.jpg",
-                "https://img.freepik.com/vector-gratis/ilustracion-planta-frailejon-diseno-plano_23-2150043617.jpg"
+                "https://img.freepik.com/vector-gratis/ilustracion-planta-frailejon-diseno-plano_23-2150043617.jpg",
+                "https://img.freepik.com/vector-gratis/ilustracion-planta-frailejon-diseno-plano_23-2150043617.jpg",
+                "https://img.freepik.com/vector-gratis/ilustracion-planta-frailejon-diseno-plano_23-2150043617.jpg",
+                "https://img.freepik.com/vector-gratis/ilustracion-planta-frailejon-diseno-plano_23-2150043617.jpg",
+                "https://img.freepik.com/vector-gratis/ilustracion-planta-frailejon-diseno-plano_23-2150043617.jpg",
                 ]
             setProyectos( urlProyectos)
 
@@ -24,26 +28,30 @@ export default function MyProyects(){
         <div className="container_projects">
 
             <div className="projects">
-            <h1> Proyectos</h1>
+
+            <div className="title">
+            <h1> Projects</h1> </div>
 
             <div className="my_projects">
                 
                 {Proyectos.map((element, index)=>{
                     return(
-                    <div>
+                    <div className="get_proyects">
                     <img className="image_projects" key={index} src={element} alt="imagenes"  />
-                    <button></button>
+
+                    <div className="button">
+                    <button className="information_projects">Information</button> 
                     </div>
+
+                </div>
                 )
                 
                 })}
-            <div className="description">
-                
-            </div>
 
             </div>
 
             </div>
+            
 
         </div>
     )
